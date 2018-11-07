6ad2f08a155134a0a6e581b95e65ade1ed7f99bc
Attasiem Kingsley <kingcorp>
2018-11-07 22:37:18 +0100

    Added a Procfile fixed untracked files

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -5,47 +5,47 @@ const User = require('../models/userModel');
 
 
 //log user in
-// exports.user_login = (req, res, next) => {
-//     User.find({email: req.body.email})
-//     .exec()
-//     .then(user =>{
-//         if(user.length > 1){
-//             return res.status(402).json({
-//                 message: 'Auth failed'
-//             })
-//         }
-//         bcrypt.compare(req.body.password, user[0].password, (err, result)=>{
-//             if(err){
-//                 return res.status(402).json({
-//                     message: 'Auth failed'
-//                 })
-//             }
-//             if(result){
-//                 const token = jwt.sign(
-//                     {email: user[0].email, userId: user[0]._id },
-//                     "secret",
-//                     { expiresIn: "1h"}
-//                 );
+exports.user_login = (req, res, next) => {
+    User.find({email: req.body.email})
+    .exec()
+    .then(user =>{
+        if(user.length > 1){
+            return res.status(402).json({
+                message: 'Auth failed'
+            })
+        }
+        bcrypt.compare(req.body.password, user[0].password, (err, result)=>{
+            if(err){
+                return res.status(402).json({
+                    message: 'Auth failed'
+                })
+            }
+            if(result){
+                const token = jwt.sign(
+                    {email: user[0].email, userId: user[0]._id },
+                    "secret",
+                    { expiresIn: "1h"}
+                );
 
-//                 return res.status(200).json({
-//                     message: 'Auth Successful',
-//                     userId: user[0]._id,
-//                     token: token
-//                 })
-//             }
-//             return res.status(402).json({
-//                 message: 'Auth failed 2'
-//             })
-//         })
-//     })
-//     .catch(err =>{
-//         console.log(err)
-//         res.status(402).json({
-//             error: err,
-//             message: 'Email or Password INVALID',
-//         })
-//     })
-// }
+                return res.status(200).json({
+                    message: 'Auth Successful',
+                    userId: user[0]._id,
+                    token: token
+                })
+            }
+            return res.status(402).json({
+                message: 'Auth failed 2'
+            })
+        })
+    })
+    .catch(err =>{
+        console.log(err)
+        res.status(402).json({
+            error: err,
+            message: 'Email or Password INVALID',
+        })
+    })
+}
 
 
 //register user
