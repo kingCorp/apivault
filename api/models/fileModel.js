d2ed5b458743b52e7dc2f81bff80f3cb2a7ccb68
Attasiem Kingsley <kingcorp>
2018-11-09 07:47:49 +0100

    Add a Procfile and files route responses

--- api/models/fileModel.js
+++ api/models/fileModel.js
@@ -4,9 +4,9 @@ const filesSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     userId: {type: String, required: true},
     filePath : {type: String, required: true},
-    fileName : {type: String, required: true},
-    fileType : {type: String, required: true},
-    fileSize : {type: String, required: true},
+    fileName : {type: String, },
+    fileType : {type: String, },
+    fileSize : {type: String, },
 });
 
 module.exports = mongoose.model('VaultFile', filesSchema);
